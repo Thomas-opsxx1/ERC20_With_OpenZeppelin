ff0081f64a9d11b8d2154bae01992c046562a22f
Thomas-opsxx1 <defiauditor01@gmail.com>
2024-10-18 18:14:26 +0530

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+
+    string public tokenName = "Binance Coin";
+    string public tokenSymbol = "BNB";
+    
+    constructor() ERC20(tokenName, tokenSymbol) {}
+
+    function mint(uint256 _amount) public {
+        _mint(msg.sender, _amount);
+    }
+
+    function burn(uint256 _amount) public {
+        _burn(msg.sender, _amount);
+    }
+    
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+}
